5554e296c167754194f428812a562ad0777afb97
Daniel <danylarayamamoto@hotmail.com>
2019-12-09 06:38:19 -0600

    Add comments and parentheses
    *Fix x1 and x2

--- GeneralFormula.py
+++ GeneralFormula.py
@@ -1,14 +1,14 @@
 import math
 
-a = float(input("Introduzca el valor de A: "))
-b = float(input("Introduzca el valor de B: "))
-c = float(input("Introduzca el valor de C: "))
+a = float(input("Introduzca el valor de A: ")) # Coeficiente al cuadrado
+b = float(input("Introduzca el valor de B: ")) # Coeficiente linel
+c = float(input("Introduzca el valor de C: ")) # Coeficiente independiente
 
 if a != 0 and (math.pow(b, 2) > (4 * a * c)):
     d = math.sqrt(math.pow(b, 2) - 4 * a * c)
 
-    x1 = (-b + d) / 2 * a
-    x2 = (-b - d) / 2 * a
+    x1 = (-b + d) / (2 * a)
+    x2 = (-b - d) / (2 * a)
         
     print("La raíz de X1: ", x1)
     print("La raíz de x2: ", x2)
